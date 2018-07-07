a8d8d986f254575d76911910f0a402427e178a72
seangz <sean.gnanarajah@gmail.com>
2018-07-07 16:48:34 -0400

    iot_config

---
+++ iot_config.sh
@@ -0,0 +1,1 @@
+python cloudiot_mqtt_example_json.py --project_id=iot-seang --cloud_region=us-central1 --registry_id=iot-seang --device_id=device1 --private_key_file=rsa_private.pem --message_type=event --algorithm=RS256 > device1.txt 2>&1 &
