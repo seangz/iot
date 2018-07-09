f0ec82cd0cbda04636a357a0862e532a963ad543
seangz <sean.gnanarajah@gmail.com>
2018-07-09 18:22:09 -0400

    t

--- iot_config.sh
+++ iot_config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export PROJECT_ID=iot-seang
-export MY_REGION=uscentral-1
+export MY_REGION=us-central1
 
 gcloud beta iot devices create nj-facility-1 \
   --project=$PROJECT_ID \
