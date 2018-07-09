d17c90856a9427eade0c97b9c4f1a8c2f83d867e
seangz <sean.gnanarajah@gmail.com>
2018-07-09 19:34:01 -0400

    t

--- iot_config.sh
+++ iot_config.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 
-export PROJECT_ID=iot-seang
-export MY_REGION=us-central1
-
 gcloud beta iot devices create nj-facility-1 \
-  --project=$PROJECT_ID \
-  --region=$MY_REGION \
+  --project=iot-seang \
+  --region=us-central1 \
   --registry=iotlab-registry \
   --public-key path=rsa_cert.pem,type=rs256
 
 gcloud beta iot devices create nj-facility-2 \
-  --project=$PROJECT_ID \
-  --region=$MY_REGION \
+  --project=iot-seang \
+  --region=us-central1 \
   --registry=iotlab-registry \
   --public-key path=rsa_cert.pem,type=rs256
 
-gcloud beta iot devices create ny-facility-1 \
-  --project=$PROJECT_ID \
-  --region=$MY_REGION \
+gcloud beta iot devices create nj-facility-3 \
+  --project=iot-seang \
+  --region=us-central1 \
   --registry=iotlab-registry \
   --public-key path=rsa_cert.pem,type=rs256
--- simulate.sh
+++ simulate.sh
@@ -1,28 +1,7 @@
 #!/usr/bin/env bash
 
-python iot_simulator.py \
-   --project_id=$PROJECT_ID \
-   --cloud_region=$MY_REGION \
-   --registry_id=iotlab-registry \
-   --device_id=nj-facility-1 \
-   --private_key_file=rsa_private.pem \
-   --message_type=event \
-   --algorithm=RS256 > nj-facility-1.txt 2>&1 &
+python iot_simulator.py --project_id=iot-seang --cloud_region=us-central1 --registry_id=iotlab-registry --device_id=nj-facility-1 --private_key_file=rsa_private.pem --message_type=event --algorithm=RS256 > nj-facility-1.txt 2>&1 &
 
-python iot_simulator.py \
-   --project_id=$PROJECT_ID \
-   --cloud_region=$MY_REGION \
-   --registry_id=iotlab-registry \
-   --device_id=nj-facility-2 \
-   --private_key_file=rsa_private.pem \
-   --message_type=event \
-   --algorithm=RS256 > nj-facility-2.txt 2>&1 &
+python iot_simulator.py --project_id=iot-seang --cloud_region=us-central1 --registry_id=iotlab-registry --device_id=nj-facility-2 --private_key_file=rsa_private.pem --message_type=event --algorithm=RS256 > nj-facility-2.txt 2>&1 &
 
-python iot_simulator.py \
-      --project_id=$PROJECT_ID \
-      --cloud_region=$MY_REGION \
-      --registry_id=iotlab-registry \
-      --device_id=ny-facility-1 \
-      --private_key_file=rsa_private.pem \
-      --message_type=event \
-      --algorithm=RS256 > ny-facility-1.txt 2>&1 &
+python iot_simulator.py --project_id=iot-seang --cloud_region=us-central1 --registry_id=iotlab-registry --device_id=nj-facility-3 --private_key_file=rsa_private.pem --message_type=event --algorithm=RS256 > nj-facility-3.txt 2>&1 &
