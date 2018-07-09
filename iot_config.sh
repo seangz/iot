36816a3612a7f8004b4f3081a1c6be096b5af83e
seangz <sean.gnanarajah@gmail.com>
2018-07-09 18:13:40 -0400

    Update

--- iot_config.sh
+++ iot_config.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+export PROJECT_ID=iot-seang
+export MY_REGION=uscentral-1
 
-gcloud beta iot devices create temp-sensor-buenos-aires \
+gcloud beta iot devices create nj-facility-1 \
+  --project=$PROJECT_ID \
+  --region=$MY_REGION \
+  --registry=iotlab-registry \
+  --public-key path=rsa_cert.pem,type=rs256
+
+gcloud beta iot devices create nj-facility-2 \
+  --project=$PROJECT_ID \
+  --region=$MY_REGION \
+  --registry=iotlab-registry \
+  --public-key path=rsa_cert.pem,type=rs256
+
+gcloud beta iot devices create ny-facility-1 \
   --project=$PROJECT_ID \
   --region=$MY_REGION \
   --registry=iotlab-registry \
